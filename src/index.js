7dcb205cb84b0daf5fb4fe2b429427c0557dcd6e
xstartstaticx <daniel.reiller@icloud.com>
2022-10-11 13:25:36 +0200

    finished exercise

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 export const PrintHello = () => {
-	return <h1>I Love React</h1>;
+  return <h1>I Love React</h1>;
 };
 
-// change the syntaxt of the first parameter to make it <PrintHello /> instead of PrintHello()
-ReactDOM.render(PrintHello(), document.querySelector("#root"));
+// change the syntax of the first parameter to make it <PrintHello /> instead of PrintHello()
+ReactDOM.render(<PrintHello />, document.querySelector("#root"));
